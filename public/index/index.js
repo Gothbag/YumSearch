06a57e6caf827b018d71e2df4becad763eacda33
ChemaGAGM <josemgagm@gmail.com>
2016-04-14 21:05:30 +0200

    Sign up menu. Terms and conditions modal. Provisional logo

--- public/index/index.js
+++ public/index/index.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
 	$("#navLogIn").click(function() {
 		$("#logInModal").modal();
 	});
-
+    $("#navSignUp").click(function() {
+		$("#signUpModal").modal();
+	});
+    $("#terms_cond").click(function() {
+		$("#t_and_c_m").modal();
+	});
 });
 
 /* index Knockout*/
