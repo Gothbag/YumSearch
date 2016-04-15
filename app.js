3ade3369ccc9a9857f734f0ef37d5e58d40a1208
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-15 17:15:51 +0200

    installed new packages

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +23,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret: 'yums34rch1smys3cr3t', saveUninitialized: true, resave: true}));
 
 app.use('/', routes);
 app.use('/users', users);
