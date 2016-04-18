fbf81542e2f47729ef7aa9a38f9b2576f6c03fed
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-18 18:30:35 +0200

    added passport sign up

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,15 @@ module.exports = function (passport) {
 	  res.send('respond with a resource');
 	});
 
+	router.post('/signup', function (req, res, next) {
+		passport.authenticate('local-signup', function (req, res, next) {
+			if (err) { return next(err); }
+		    if (!user) { return res.json({success:false}); }
+	    	return res.json({success: true});
+		})(req, res, next);
+
+	});
+
 	return router;
 
 };
