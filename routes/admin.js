e3bbe7222b27b7405abff0414533bea864d11a80
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-20 20:28:35 +0200

    added req.user

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/admin', function(req, res, next) {
-  res.render('pages/admin/adminMain.ejs', { title: 'Home' });
+  res.render('pages/admin/adminMain.ejs', { title: 'Home', user: req.user });
 });
 
 module.exports = router;
--- routes/business.js
+++ routes/business.js
@@ -2,7 +2,7 @@ module.exports = function (app, passport) {
 
 	/* GET home business page. */
 	app.get('/business', function(req, res, next) {
-        res.render('pages/business/businessMain.ejs', { title: 'Business' });
+        res.render('pages/business/businessMain.ejs', { title: 'Business', user: req.user });
 	});
 
 };
