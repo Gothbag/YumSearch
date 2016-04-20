1f309a3633b247a866709fdbdfa2b8373ef3cd5b
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-20 16:47:33 +0200

    added more stuff

---
+++ models/user.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+
+// define the schema for our user model
+var userSchema = mongoose.Schema({
+
+    local            : {
+        email        : String,
+        username     : String,
+        password     : String,
+    },
+    facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    },
+    twitter          : {
+        id           : String,
+        token        : String,
+        displayName  : String,
+        username     : String
+    },
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    }
+
+});
+
+//extra methods for the schema
+userSchema.methods.generateHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8)); //synchronous hashing
+}
+
+userSchema.methods.validPassword = function (password) {
+    return bcrypt.compareSync(password, this.local.password); //synchronous password comparison
+}
+
+module.exports = mongoose.model('User', userSchema);
---
+++ public/resources/js/general.js
@@ -0,0 +1,12 @@
+/* events */
+$(document).ready(function () {
+	$("#navLogIn").click(function() {
+		$("#logInModal").modal();
+	});
+    $("#navSignUp").click(function() {
+		$("#signUpModal").modal();
+	});
+    $("#terms_cond").click(function() {
+		$("#t_and_c_m").modal();
+	});
+});
