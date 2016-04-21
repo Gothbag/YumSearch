3e1da00b8ab9ce20cb15f4a6a8e9fc941003dc49
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-21 19:55:59 +0200

    added jquery validation

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,8 @@ module.exports = function (app, passport) {
 	  res.render('pages/main/index.ejs', { title: 'Home', user: req.user });
 	});
 
-	/* GET users listing. */
-	app.get('/users',isAuthenticated, function(req, res, next) {
-	  res.send('respond with a resource');
-	});
+
 
 
 };
 
-//function to verify the user is logged in
-var isAuthenticated = function (req, res, next) {
-	// if user is authenticated in the session, call the next() to call the next request handler
-	// Passport adds this method to request object. A middleware is allowed to add properties to
-	// request and response objects
-	if (req.isAuthenticated()) { return next();}
-	//if the user is not authenticated then we redirect them to the main page
-	res.redirect('/');
-
-}
