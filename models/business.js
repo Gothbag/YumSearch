1c302391ab9d522e8b04907e9bb102149350e632
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-25 17:24:24 +0200

    replaced old package with new one

--- models/business.js
+++ models/business.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcrypt-nodejs');
+var bcrypt = require('bcryptjs');
 
 // define the schema for our user model
 var businessSchema = mongoose.Schema({
