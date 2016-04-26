1cc585f4d79db1f09d65eef9e630f79695cc0a92
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-04-26 18:44:01 +0200

    worked on the edit profile business logic

--- public/resources/js/general.js
+++ public/resources/js/general.js
@@ -4,7 +4,22 @@ $(document).ready(function () {
 		$("#t_and_c_m").modal();
 	});
 
+    /* function to validate names with non-ASCII characters*/
+    $.validator.addMethod("LatinNames", function (value, element) {
+       return (/^[a-z\u00E0-\u00FC\s-]+$/i.test(value));
+    }, "Incorrect format");
 
+    $.validator.addMethod("skip_or_fill_minimum", function(value, element, options) {
+        var elems = $(element).parents('form').find(options[1]);
+        var numberFilled = elems.filter(':filled').size();
+        if (numberFilled >= options[0] || numberFilled == 0) {
+            elems.removeClass('error');
+            elems.next('label.error').not('.checked').text('').addClass('checked');
+            return true;
+        } else {
+            elems.addClass('error');
+        }
+    }, $.format("Please either skip these fields or fill at least {0} of them."));
 });
 
 // override jquery validate plugin defaults
@@ -25,7 +40,22 @@ $.validator.setDefaults({
         }
     }
 });
-/* function to validate names with non-ASCII characters*/
-$.validator.addMethod("LatinNames", function (value, element) {
-   return (/^[a-z\u00E0-\u00FC\s-]+$/i.test(value));
-}, "Incorrect format");
+
+//this a new method to JQuery to serialize an object
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
