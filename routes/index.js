ae085c813b6ced98e723202cfe80557421ec8d67
ChemaGAGM <josemgagm@gmail.com>
2016-04-26 17:37:52 +0200

    added contact page

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,9 @@ module.exports = function (app, passport) {
 	  res.render('pages/main/index.ejs', { title: 'Home', user: req.user });
 	});
 
+    /*GET contact page*/
+    app.get('/contact', function(req, res, next) {
+        res.render('pages/main/contact.ejs', { title: 'Contact', user: req.user });
+	});
 };
 
