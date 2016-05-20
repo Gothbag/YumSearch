f32d0355aefc890ff7d80a37ccd345447e9cf51e
Josep Maria Cagigal Soler <josemariacagigalso@gmail.com>
2016-05-20 20:11:22 +0200

    worked on business search page

--- public/businesssearch/search.js
+++ public/businesssearch/search.js
@@ -1,82 +1,146 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <% include ../../partials/head %>
-    </head>
+var enlargedMap;
+$(document).ready(function () {
 
-    <body>
+    /*$('#mapModal').on('show.bs.modal', function() {
+       //Must wait until the modal shows, that's why we require a callback funtion
+       resizeMap();
+    });*/
 
-        <% include ../../partials/navigation %>
+    if ($("#LargeMap").length > 0) { //this is so the script doesn't cause an exception if the selector cannot be found
+        //the map center is in Barcelona
+        var barcelona = new google.maps.LatLng(41.3833, 2.1833);
+        function initialize() {
+            var mapOptions = {
+                zoom: 15,
+                mapTypeId: google.maps.MapTypeId.ROADMAP,
+                center: barcelona
+            };
+            enlargedMap = new google.maps.Map(document.getElementById("LargeMap"), mapOptions);
 
-        <div class="jumbotron jumbotron-sm"><br>
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-12 col-lg-12">
-                        <h1 class="h1">
-                            <img class="img-responsive img-thumbnail img-circle" src="/resources/img/rounded_white_icon.png" /> Search Businesses</h1>
-                    </div>
-                </div>
-            </div>
-        </div>
+        }
+        google.maps.event.addDomListener(window, "load", initialize);
+    }
 
-        <!--Offers management-->
-        <div class="container">
-            <div class="row"  id="offers">
-                <div class="col-md-12">
-                    <input id="searchBusiness" name="searchBusiness" type="text" />
-                    <form id="form2" action="#">
+    //we update the search placeholder text
+    $("#maxDistance").change(function () {
 
-                        <h3>Search businesses </h3><br/>
+        $("#MainSearch").attr("placeholder", "Search for food products " + $("#maxDistance").val() + " km away from you.");
 
-                        <div class="table-responsive">
+    });
+});
+
+/* index Knockout*/
+
+(function () {
+
+    var ENTER_KEY = 13;
+
+    // A factory function we can use to create binding handlers for specific
+    // keycodes (which we'll use for the Enter key).
+    function keyhandlerBindingFactory(keyCode) {
+        return {
+            init: function (element, valueAccessor, allBindingsAccessor, data, bindingContext) {
+                var wrappedHandler, newValueAccessor;
+
+                // wrap the handler with a check for the enter key
+                wrappedHandler = function (data, event) {
+                    if (event.keyCode === keyCode) {
+                        valueAccessor().call(this, data, event);
+                    }
+                };
+
+                // create a valueAccessor with the options that we would want to pass to the event binding
+                newValueAccessor = function () {
+                    return {
+                        keyup: wrappedHandler
+                    };
+                };
+
+                // call the real event binding's init function
+                ko.bindingHandlers.event.init(element, newValueAccessor, allBindingsAccessor, data, bindingContext);
+            }
+        };
+    }
+
+    // a custom binding to handle the Enter key
+    ko.bindingHandlers.enterKey = keyhandlerBindingFactory(ENTER_KEY);
+
+    ko.bindingHandlers.offerMap = {
+        init: function (element, valueAccessor) {
+            var
+            offer = valueAccessor(),
+                latLng = new google.maps.LatLng(offer.loc[1], offer.loc[0]),
+                mapOptions = {
+                    center: latLng,
+                    zoom: 14,
+                    disableDefaultUI: true,
+                    scrollwheel: false,
+                    navigationControl: false,
+                    mapTypeControl: false,
+                    scaleControl: false,
+                    draggable: false,
+                    styles: [
+                        {
+                            "featureType": "poi",
+                            "stylers": [
+                                { "visibility": "off" }
+                            ]
+                        }
+                    ],
+                    mapTypeId: google.maps.MapTypeId.ROADMAP
+                },
+                map = new google.maps.Map(element, mapOptions),
+                marker = new google.maps.Marker({
+                    position: latLng,
+                    map: map
+                }),
+                offerInfoDiv = document.createElement('div'),
+                offerInfo = new OfferInfo(offerInfoDiv, map, offer);
+            //adding the control onto the map
+            map.controls[google.maps.ControlPosition.TOP_LEFT].push(offerInfoDiv);
+        }
+    };
+
+    //the main view model
+    var ViewModel = function () {
+        var self = this;
+
+        var limit = true;
+
+        this.businesses = [];
+
+        this.loadBusinesses = function () {
+            $.ajax({
+                type: "POST",
+                url: '/business/search', /* url of the request */
+                data:JSON.stringify({search:$("#MainSearch").val()}),
+                contentType: "application/json; charset=utf-8",
+                dataType: 'json',
+                success: function (data) {
+                    self.offers.removeAll(); //all offers are removed beforehand
+                    data.map(function (offer) {
+                        limit = false;
+                        self.offers.push(new Offer(offer));
+                    });
+                }
+            });
+        }.bind(this);
+
+        this.businesses = ko.observableArray();
+        this.loadBusinesses();
+
+    };
+
+    var viewModel = new ViewModel();
+    ko.applyBindings(viewModel);
+
+}());
+
+var Business = function (business) {
+    var self = this;
+    this.address = business.address;
+    this.avgRating = business.avgRating;
+    this.name = business.name;
+}
 
 
-                            <table id="mytable" class="table table-bordred table-striped">
-
-                                <thead>
-                                    <th>Product Name</th>
-                                    <th>Price Before (€/kg)</th>
-                                    <th>Price Now (€/kg)</th>
-                                    <th>Discount (%)</th>
-
-                                    <th>Delete Offer</th>
-                                </thead>
-                                <tbody>
-                                    <!-- ko foreach: offers -->
-                                    <tr data-bind="visible: visible">
-                                        <td><input type="text" class="form-control col-lg-3 col-md-3 col-sm-3 col-xs-3 " data-bind="value: name" /></td>
-                                        <td><input type="text" class="form-control col-lg-3 col-md-3 col-sm-3 col-xs-3 Number" data-bind="value: priceBefore" /></td>
-                                        <td><input type="text" class="form-control col-lg-3 col-md-3 col-sm-3 col-xs-3 Number" data-bind="value: priceNow" /></td>
-                                        <td><input type="text" class="form-control col-lg-3 col-md-3 col-sm-3 col-xs-3 Digit" data-bind="value: differencePercentage" /></td>
-                                        <td class="text-center"><button class="btn btn-danger btn-xs" data-title="Delete" data-bind="click: $parent.remove" ><span class="glyphicon glyphicon-trash fa-2x"></span></button></td>
-                                    </tr>
-                                    <!-- /ko -->
-                                    <tr>
-                                        <td colspan="5" class="text-center"><button class="btn btn-info btn-xs" title="Click as many times as offers you need to add" data-bind="click: add" ><h4>Click  to add a new offer</h4><span class="glyphicon glyphicon-plus fa-3x"></span></button></td>
-                                    </tr>
-                                    <tr>
-                                        <td colspan="5" class="text-danger">Don't forget click SAVE button below to keep any changes</td>
-                                    </tr>
-                                    <tr class="OffersAdded">
-
-                                    </tr>
-                                </tbody>
-
-                            </table>
-
-                        </div>
-
-                    </form>
-                </div>
-                <div class="btn-group">
-                    <a class="btn btn-success" id="Save" data-bind="click: $root.save">Save <span class="glyphicon glyphicon-ok"></span></a>
-                    <a class="btn btn-warning" data-bind="click: $root.loadOffers">Cancel <span class="glyphicon glyphicon-remove"></span></a>
-                </div>
-            </div>
-        </div>
-
-        <% include ../../partials/footer %>
-        <script type="text/javascript" src="/offers/offers.js"></script>
-
-    </body>
-</html>
